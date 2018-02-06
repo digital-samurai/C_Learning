cdb9155013e95a53cb25837060b729ca28a21579
Ronald Taylor Jr <TaylorRo@southcoast.org>
2018-02-05 20:28:48 -0500

    Initial Commit

---
+++ Chapter02/Ch02_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ch02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88; // storing a double in an object
+            object name = "Amir"; // storing a string in an object
+            int length1 = name.Length; // gives compile error!
+            int length2 = ((string)name).Length; // cast to access members
+        }
+    }
+}
